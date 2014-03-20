cc2e969156f30ea84e7b0fedbbc48ba2f36feb98
Daniel Lin <linonmail@gmail.com>
2014-03-20 10:39:21 -0700

    Add ability to update users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,13 +28,12 @@ describe "AuthenticationPages" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email", with: user.email.upcase;
-        fill_in "Password", with: user.password;
-        click_button "Sign in"
+        sign_in(user)
       end
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
@@ -43,6 +42,5 @@ describe "AuthenticationPages" do
         it { should have_link('Sign in') }
       end
     end
-
   end
 end
