4e7acf30c0aa109893d0e3dae4827dbf07b769e8
Daniel Lin <dan.lin@sta.samsung.com>
2014-03-20 17:59:20 -0700

    Complete chapter 9 excercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: [:destroy]
 
   def new
+    redirect_to root_path if signed_in?
     @user = User.new
   end
 
@@ -16,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    redirect_to root_path if signed_in?
     @user = User.new(user_params)
     if @user.save
       sign_in @user
